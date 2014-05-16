9d837566d8137952f011130c46fd88e202f4aab8
Julia Evans <julia@jvns.ca>
2014-05-16 11:17:46 -0400

    Make bytesum work

--- bytesum.go
+++ bytesum.go
@@ -2,21 +2,28 @@
 // Click here and start typing.
 package main
 
-import "bytes"
 import "fmt"
 import "os"
 import "syscall"
 
 func main() {
-	fmt.Println("Hello, 世界")
     buffer, _, err := GetFileBuffer(os.Args[1]);
     if err != nil {
         panic(err)
     }
-	fmt.Println(buffer)
+    result := sumBytes(buffer)
+    fmt.Println(result)
 }
 
-func GetFileBuffer(path string) (*bytes.Buffer, int64, error) {
+func sumBytes(data []byte) uint8 {
+    x := 0
+    for b := range data {
+        x += b
+    }
+    return uint8(x)
+}
+
+func GetFileBuffer(path string) ([]byte, int64, error) {
   f, err := os.Open(path)
   if err != nil {
     return nil, 0, err
@@ -27,11 +34,9 @@ func GetFileBuffer(path string) (*bytes.Buffer, int64, error) {
   }
 
   mmap, err := syscall.Mmap(int(f.Fd()), 0, int(fin.Size()),
-syscall.PROT_READ, syscall.MAP_PRIVATE)
+      syscall.PROT_READ, syscall.MAP_PRIVATE)
   if err != nil {
     return nil, 0, err
   }
-  buffer := bytes.NewBuffer(mmap)
-
-  return buffer, fin.Size(), nil
+  return mmap, fin.Size(), nil
 }
